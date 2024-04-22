42daa3488f95d6fb7d772b2181cd0c3e273a8aa0
unknown <mfuentes85@gmail.com config --global user.name Viridianagit config --global user.email mfuentes85@gmail.com>
2024-04-21 22:41:15 -0600

    Mi primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PnturasPatitoMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
